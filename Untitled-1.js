88ff0076954525526e4d4177d5f45a3e72d13c4a
allankimanzi2 <allankim457@gmail.com>
2023-12-12 17:31:46 +0300

    DOne assignment

---
+++ Untitled-1.js
@@ -0,0 +1,26 @@
+// Function to check if a number is even or odd
+function checkEvenOrOdd(n) {
+  if (n % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+// Example usage
+let userInput = prompt("Enter a number:");
+let result = checkEvenOrOdd(parseInt(userInput));
+
+console.log(`The number is ${result}.`);
+<img src="https://s3.amazonaws.com/ironboard-learn/lion.jpg"/>
+
+<img
+  src="https://s3.amazonaws.com/ironboard-learn/lion.jpg"/>
+  alt="a lion yoaning"
+/> 
+
+<img
+  src="https://s3.amazonaws.com/ironboard-learn/lion.jpg"
+  alt="a lion yoaning"
+  title="Lion Picture"
+/>
